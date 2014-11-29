d7971f6117595b5849794b7e7fe137c19926d48c
Craig Horsborough <craighorsborough@gmail.com>
2014-11-29 15:03:43 +0000

    Changed background color & started on questions page

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.listen(port);
 app.set('views',__dirname + '/app')
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
+console.log("Listening on port 3000");
 app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
