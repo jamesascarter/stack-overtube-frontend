39b4feab3ba63c8866ab51db00dcc6b7c40cac4f
Craig Horsborough <craighorsborough@gmail.com>
2014-12-04 11:29:41 +0000

    Made all question fields compulsary with angular

--- app/scripts/controllers/askquestions.js
+++ app/scripts/controllers/askquestions.js
@@ -3,6 +3,9 @@
 app.controller('AskquestionsCtrl', function ($scope, $http, $location) {
 
   $scope.addQuestion = function () {
+
+if ($scope.myform.$valid) {
+
     console.log($scope.title);
     console.log($scope.description);
     console.log($scope.codesnippet);
@@ -23,6 +26,10 @@ app.controller('AskquestionsCtrl', function ($scope, $http, $location) {
     	// 	alert("An error has ocurred!")
     	// });
     $location.path('/questions');
+  }
+  else {
+    console.log("Form not valid");
+    }
   };
 
 });
