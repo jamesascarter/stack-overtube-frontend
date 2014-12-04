85aa3c94217164748b970b4549739ec01255af24
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-12-04 12:34:55 +0000

    Alll questions show number of videos

--- app/scripts/controllers/allquestions.js
+++ app/scripts/controllers/allquestions.js
@@ -2,11 +2,28 @@
 
 angular.module('stackOverTubeApp').controller('AllquestionsCtrl', function ($scope,$http, $location) {
 
+  // var getQuestions = function () {
+  //   return $http.get('https://overtube-backend.herokuapp.com/allquestions').then(function(response) {
+  //     $scope.questions = response.data;
+  //   return response;
+  //  })
+  // };
+
+
+
   var getQuestions = function () {
-    return $http.get('https://overtube-backend.herokuapp.com/allquestions').then(function(response) {
-      $scope.questions = response.data;
-    return response;
-   })
+    return $http.get('https://overtube-backend.herokuapp.com/allquestions').then(function(response1) {
+      $scope.questions = response1.data;
+      var q = response1.data;
+      var arr = [];
+      for(var i = 0; i < q.length; i++){
+        $http.get('https://overtube-backend.herokuapp.com/question/'+q[i].id).then(function(response2){
+          arr.push(response2.data.reply.length)
+            console.log(arr);
+            return $scope.allreplies = arr;
+        });
+      }
+   });
   };
 
   $scope.sortorder = '-views'
