ddd8ac89dc78ffeb951d91b52ab7f5d5261be028
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-12-04 12:20:08 +0000

    Added extra details to individual question page

--- app/scripts/controllers/questionretriever.js
+++ app/scripts/controllers/questionretriever.js
@@ -7,18 +7,15 @@ angular.module('stackOverTubeApp').controller('QuestionRetrieverCtrl', function 
       $scope.questionId = $stateParams.questionId;
       return $http.get('https://overtube-backend.herokuapp.com/question/'+ $scope.questionId).then(function(response1) {
         $scope.question = response1.data.question;
-        console.log(response1.data)
         $scope.replies = $sce.getTrustedJs(response1.data.reply);
-        // $scope.replies = response1.data.reply;
-        console.log(response1.data.reply)
-        // return $http.get('https://overtube-backend.herokuapp.com/user/'+ response1.data.question.UserId).then(function(err, response2) {
-        //   if(err) {
-        //     $scope.user = {name: "No user"}
-        //   } else {
-        //     $scope.user = response2.data
-        //   };
+        return $http.get('https://overtube-backend.herokuapp.com/user/'+ response1.data.question.UserId).then(function(response2) {
+            if(response2.data.username === "") {
+                $scope.username = "Anonymous"
+            } else {
+                $scope.username =  response2.data.username;
+            }
         });
-      // });
-    };
+      });
+    }
     getBody()
 });
